98f154bfa9de8a7f8e085b2f05eb66e3fbe5fac7
vasu8480 <vasu8480@gmail.com>
2022-12-28 15:06:55 +0530

    Django

--- Django/telusko/calc/views.py
+++ Django/telusko/calc/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html', {'name': 'Vasu'})
+
+def add(request):
+    val1 = request.POST['num1']
+    val2 = request.POST['num2']
+    res = val1 + val2
+    return render(request, 'res.html', {'result': res})
